6df20a5403a997c9e0b2ad6e67e4f66fc3f65aac
Anas Mohsen <m.mohsen_18634@khi.iba.edu.pk>
2023-05-07 20:26:47 +0500

    initial commit

---
+++ app.py
@@ -0,0 +1,94 @@
+from flask import Flask, request, jsonify
+
+import spacy
+
+app = Flask(__name__)
+
+ENV = 'dev'
+
+if ENV == 'dev':
+    app.debug = True
+else:
+    app.debug = False
+
+
+# Load the en_core_sci_lg model
+nlp_ss = spacy.load("en_core_sci_lg")
+
+#Function defined for 2 marks question
+def get_similarity(ms,ans,nlp):
+  scores = []
+  s2 = nlp((ans.lower()))
+
+  for i in range(len(ms)):
+    score = 0
+    for m in ms[i]:
+      s1 = nlp(m.lower())
+      similarity_score = s1.similarity(s2)
+      print(m,similarity_score)
+      score = max(score,similarity_score)
+    print(score)
+    scores.append(score)
+  print()
+  score = sum(scores)/len(scores)
+  return score
+
+def get_similarity_binary(ms,ans,nlp):
+  scores = []
+  s2 = nlp((ans.lower()))
+
+  for i in range(len(ms)):
+    score = 0
+    for m in ms[i]:
+      s1 = nlp(m.lower())
+      similarity_score = s1.similarity(s2)
+      print(m,similarity_score)
+      score = max(score,similarity_score)
+    print(score)
+    scores.append(score)
+  print()
+  score = sum(scores)/len(scores)
+  return scores
+
+
+def get_similarity_concatenated(ms,ans,nlp):
+  score = 0
+
+  if len(ms) == 1:
+    return get_similarity(ms,ans,nlp)
+
+  s1 = nlp(ans.lower())
+  for i in range(len(ms[0])):
+    for j in range(len(ms[1])):
+      m = ms[0][i] + " " + ms[1][j]
+      s2 = nlp(m.lower())
+      similarity_score = s1.similarity(s2)
+      print(m,similarity_score)
+      score = max(score,similarity_score)
+  return score
+
+# define a function to apply to each row
+def similarity_for_spacy(row, response, ms):
+    ans = response
+    ms_index = 0
+    x = get_similarity(ms,ans,nlp_ss)
+    x = 0
+    return x
+
+@app.route('/compute-ai-score')
+def compute_ai_score():
+    body = request.get_json()
+    marking_scheme = body["markingScheme"]
+    answer = body["studentResponse"]
+    score = get_similarity(marking_scheme, answer, nlp_ss)
+    return jsonify({
+       "success": True,
+       "score": score 
+    })
+
+@app.route('/')
+def index():
+   return "Hello from RapidCheck AI API"
+
+if __name__ == '__main__':
+    app.run(host='127.0.0.1', port=8000, debug=True)
---
+++ readme.md
@@ -0,0 +1,53 @@
+# RapidCheck AI Flask API
+
+This is the API that runs the AI and outputs a similarity score.
+
+## Getting Started
+
+To get started, clone the repository:
+
+```
+git clone https://github.com/md-anas-mohsen/rapid-check-ai-backend.git
+cd flask-project
+```
+
+## Prerequisites
+
+You will need Python 3.10.11 installed on your machine. You can download it from the official website:
+
+## Installing
+
+### Create a virtual environment:
+
+```
+python -m venv venv
+Activate the virtual environment:
+```
+
+#### On Windows, run:
+
+```
+venv\Scripts\activate
+```
+
+#### On Unix or Linux, run:
+
+```
+source venv/bin/activate
+```
+
+#### Install the required packages:
+
+```
+pip install -r requirements.txt
+```
+
+#### Running the App
+
+To run the app, execute the following command:
+
+```
+flask run
+```
+
+Then, open a web browser and go to http://localhost:5000/. You should see a "Hello from RapidCheck AI API" message.
